7ae10e61b87512e967c54bb693bccc9f2b4619f8
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-06 09:01:31 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
-console.log('User flipped ' + cardTwo);
-console.log('User flipped ' + cardFour);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardsInPlay[0]);
+console.log('User flipped ' + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert('You found a match!')
+  } else {
+    alert('Sorry, try again')
+  }
+}
